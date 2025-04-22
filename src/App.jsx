3e5db3bdfc900070556e2e1a0e249210eadd1c94
alappiah <136016573+alappiah@users.noreply.github.com>
2025-04-22 22:55:56 +0000

    33

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<Navigate to="/login" replace />} />
             <Route exact path="/" element={<HomePage />}></Route>
             <Route
               exact
